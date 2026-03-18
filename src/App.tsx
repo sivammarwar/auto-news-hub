6ef8e4c774fe9ad2521e0550be4cda03d43fcb14
sivammarwar <shivamkumarsingh8544@gmail.com>
2026-03-19 03:06:37 +0530

    fixed dot 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Index from "./pages/Index";
 import ArticlePage from "./pages/ArticlePage";
 import CategoryPage from "./pages/CategoryPage";
 import AdminLogin from "./pages/AdminLogin";
-import AdminPage from "./pages/Adminpage";
+import AdminPage from "./pages/AdminPageNew";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
